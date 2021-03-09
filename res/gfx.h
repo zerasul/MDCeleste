5ce63c81493fa9da9852c0ba1eb9b652e7374371
zerasul <zerasul@gmail.com>
2021-03-09 21:26:00 +0100

    añadiendo recursos

---
+++ res/gfx.h
@@ -0,0 +1,7 @@
+#ifndef _RES_GFX_H_
+#define _RES_GFX_H_
+
+extern const Palette tileset_pal;
+extern const TileSet tileset;
+
+#endif // _RES_GFX_H_
---
+++ res/gfx.res
@@ -0,0 +1,2 @@
+TILESET tileset "tileset.png" 0
+PALETTE tileset_pal "tileset.png" 0
